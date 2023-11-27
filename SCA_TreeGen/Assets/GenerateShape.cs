d8b2d45f9489cac6207752167c48bc368a172b64
Bonita Woralert <67742281+BonitaWoralert@users.noreply.github.com>
2023-11-27 15:12:14 +0000

    (read description) Some issues, but closer to functioning
    - Every update, tree creates duplicate branches
    - Tree skeleton generates but ignores Z axis
    - Added togglePointsView for better visibility
    - Now correctly finding vector between influence points and branches
    - added more comments

--- SCA_TreeGen/Assets/GenerateShape.cs
+++ SCA_TreeGen/Assets/GenerateShape.cs
@@ -6,26 +6,22 @@ using static UnityEngine.Mesh;
 using UnityEngine.UIElements;
 
 public class GenerateShape : MonoBehaviour
-{ 
+{
+    [Header("Gizmos")]
+    [SerializeField] private bool TogglePointsView = false;
+
     [Header("Crown Variables (sphere)")]
     [SerializeField][Range(0,25)] private float radius = 8;
     [SerializeField][Range(0,100)] private int sliceCount = 10, stackCount = 10;
     Mesh mesh;
     List<Vector3> verticesList = new List<Vector3>();
 
-    //attraction points
-    private List<Vector3> attractionPoints = new List<Vector3>();
-    [Header("Attraction Points")]
-    [SerializeField] private bool displayKillDistance = false;
-    [SerializeField][Range(0, 10)] private float killDistance = 1f;
-    [SerializeField] private bool displayInfluenceDistance = false;
-    [SerializeField][Range(0,10)]private float influenceDistance = 2f;
-
     //branches
 
     [Header("Branches")]
-    [SerializeField] private List<Branch> branches = new List<Branch>();
-    [SerializeField] private List<Branch> newBranches = new List<Branch>();
+    private List<Branch> branches = new List<Branch>();
+    private List<Branch> newBranches = new List<Branch>();
+    [SerializeField] int numOfBranches;
 
     class Branch
     {
@@ -33,10 +29,7 @@ public class GenerateShape : MonoBehaviour
         public Vector3 endPos;
         public Vector3 direction;
         public List<Vector3> pointsInRange = new List<Vector3>();
-        public Branch previousBranch;
-        public List<Branch> nextBranches = new List<Branch>();
-        public bool finishedGrowing;
-        public float branchLength = 0.5f;
+        public float branchLength = 1.5f;
 
         public Branch(Vector3 startPos, Vector3 direction)
         {
@@ -45,55 +38,123 @@ public class GenerateShape : MonoBehaviour
         }
     }
 
+    //attraction points
+    //private List<Vector3> attractionPoints = new List<Vector3>();
+    private List<AttractionPoints> attractionPoints = new List<AttractionPoints>();
+    [Header("Attraction Points")]
+    [SerializeField] private bool displayKillDistance = false;
+    [SerializeField][Range(0, 10)] private float killDistance = 1f;
+    [SerializeField] private bool displayInfluenceDistance = false;
+    [SerializeField][Range(0, 10)] private float influenceDistance = 2f;
+
+    class AttractionPoints
+    {
+        public Branch closestBranch;
+        public Vector3 position;
+
+        public AttractionPoints(Vector3 position)
+        { this.position = position; }
+    }
+    
+
     private void Start()
     {
         DrawSphere();
         GenerateAttractionPoints();
 
         //Branch firstBranch = new Branch(new Vector3(0,-9,0), new Vector3(0, branchLength-9, 0), Vector3.up);
-        Branch firstBranch = new Branch(new Vector3(0, -8, 0), Vector3.up);
+        Branch firstBranch = new Branch(new Vector3(0, -11, 0), Vector3.up);
         branches.Add(firstBranch);
     }
 
     private void Update()
     {
-        foreach (Branch branch in newBranches)
-            branch.finishedGrowing = true;
+        numOfBranches = branches.Count;
 
-        for(int i = 0;i<attractionPoints.Count;i++)
+        //attraction points pick nearest branch node 
+        foreach(var a in attractionPoints)
         {
+            foreach(Branch b in branches)
+            {
+                //find distance between the point and the branch
+                float distance = Vector3.Distance(b.endPos, a.position);
+                //if its in influence distance and closer than current closest branch (or if theres no closest branch) then this branch is now closest
+                if(distance < influenceDistance && a.closestBranch == null)
+                {
+                    a.closestBranch = b;
+                    //Debug.Log("attraction point at " + a.position + " closest branch = " + b.endPos);
+                }
+                else if(distance < influenceDistance && distance < Vector3.Distance(a.position, a.closestBranch.endPos))
+                {
+                    a.closestBranch = b;
+                    //Debug.Log("attraction point at " + a.position + " closest branch = " + b.endPos);
+                }
+            }
+        }
+
+        //tell branches which points are affecting them
+        foreach(var a in attractionPoints)
+        {
+            if(a.closestBranch != null)
+            {
+                //a.closestBranch.pointsInRange.Clear(); //clear points in range
+                a.closestBranch.pointsInRange.Add(a.position);
+            }
+        }
+
+        foreach(Branch b in branches)
+        {
+            if(b.pointsInRange.Count > 0) //if the branch has influence points
+            {
+                //Debug.Log("Branch at " + b.endPos + " has " + b.pointsInRange.Count + " points in range");
+                Vector3 newBranchDir = Vector3.zero;
+                Vector3 branchToPoint = Vector3.zero;
+                for(int i = 0; i < b.pointsInRange.Count;i++)
+                {
+                    //get normalised vector from tree node to each influencing point
+                    branchToPoint = b.pointsInRange[i] - b.endPos;
+                    branchToPoint.Normalize();
+                    //add together
+                    newBranchDir += branchToPoint;
+                }
+                //then normalize again
+                newBranchDir.Normalize();
+
+                //add new branches
+                newBranches.Add(new Branch(b.endPos, newBranchDir));
+                Debug.Log("New branch is being added: startPos = " + b.endPos + " \nDirection = " +  newBranchDir);
+            }
+        }
+
+        //add these new nodes, remove points, repeat.
+        branches.AddRange(newBranches);
+
+        foreach (Branch b in branches)
+        {
+            b.pointsInRange.Clear(); //clear points in range
+            //Debug.Log("points in range cleared");
+        }
+        newBranches.Clear();
+
+
+        //remove attraction points that have been reached
+        for (int i = 0; i < attractionPoints.Count; i++)
+        {
+            //also reset closest branch
+            attractionPoints[i].closestBranch = null;
             foreach (Branch branch in branches)
             {
-                if (Vector3.Distance(branch.endPos, attractionPoints[i]) < killDistance) //if a branch is in the kill distance
+                if (Vector3.Distance(branch.endPos, attractionPoints[i].position) < killDistance) //if a branch is in the kill distance
                 {
-                    Debug.Log("attraction point " + attractionPoints[i] + " has been removed.");
+                    Debug.Log("attraction point at " + attractionPoints[i].position + " has been removed.");
                     attractionPoints.Remove(attractionPoints[i]); //remove that point
                     break;
-                } 
+                }
             }
         }
 
-        /*if (Vector3.Distance(branch.endPos, points) < influenceDistance) //if there are any points in range of the branch then add to pointsInRange
-                {
-                    branch.pointsInRange.Add(points);
-                    for (int i = 0; i < branch.pointsInRange.Count; i++)
-                        Debug.Log(branch.pointsInRange[i]);
-                }*/
+        //stop when all points removed / not in range
 
-        /*foreach (Branch branch in branches) //create new branches
-        {
-            Vector3 totalDir = Vector3.zero;
-            for(int i = 0;i < branch.pointsInRange.Count;i++)
-            {
-                totalDir += branch.pointsInRange[i];
-            }
-            totalDir.Normalize();
-
-            newBranches.Add(new Branch(branch.endPos, totalDir));
-        }
-
-        //add new branch to total branch thing 
-        branches.AddRange(newBranches);*/
     }
 
     private void DrawSphere()
@@ -197,32 +258,38 @@ public class GenerateShape : MonoBehaviour
     private void GenerateAttractionPoints()
     {
         //Vertex method: Attraction points are placed at each vertex of the crown
-        attractionPoints = verticesList;
+        for(int i = 0; i < verticesList.Count;  i++)
+        {
+            attractionPoints.Add(new AttractionPoints(verticesList[i]));
+        }
     }
 
     private void OnDrawGizmos()
     {
-        //draw crown
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireMesh(mesh);
-
-        //draw attraction points
-        foreach (var attraction in attractionPoints)
+        if(TogglePointsView == false)
         {
-            Gizmos.color = Color.white;
-            Gizmos.DrawWireSphere(attraction, 0.5f);
+            //draw crown
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireMesh(mesh);
 
-            //draw influence + kill distance
-            if (displayKillDistance == true)
+            //draw attraction points
+            foreach (var attraction in attractionPoints)
             {
-                Gizmos.color = Color.red;
-                Gizmos.DrawWireSphere(attraction, killDistance);
-            }
+                Gizmos.color = Color.white;
+                Gizmos.DrawWireSphere(attraction.position, 0.5f);
+
+                //draw influence + kill distance
+                if (displayKillDistance == true)
+                {
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawWireSphere(attraction.position, killDistance);
+                }
             
-            if (displayInfluenceDistance == true) 
-            { 
-                Gizmos.color = Color.blue;
-                Gizmos.DrawWireSphere(attraction, influenceDistance);
+                if (displayInfluenceDistance == true) 
+                { 
+                    Gizmos.color = Color.blue;
+                    Gizmos.DrawWireSphere(attraction.position, influenceDistance);
+                }
             }
         }
 
@@ -231,6 +298,7 @@ public class GenerateShape : MonoBehaviour
         {
             Gizmos.color = Color.yellow;
             Gizmos.DrawWireSphere(branch.startPos, 0.2f);
+            Gizmos.color = Color.magenta;
             Gizmos.DrawWireSphere(branch.endPos, 0.2f);
         }
     }
