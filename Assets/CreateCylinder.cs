9af9fd70c2c99bbe193b80cd5636bb8cab6d4f29
Echosaii <67742281+BonitaWoralert@users.noreply.github.com>
2024-02-04 13:41:34 +0000

    circle is generated from branch with specified radius + slices :+1:

--- Assets/CreateCylinder.cs
+++ Assets/CreateCylinder.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 using static UnityEngine.Mesh;
 
 //https://apparat-engine.blogspot.com/2013/04/procdural-meshes-cylinder.html
@@ -11,6 +12,7 @@ using static UnityEngine.Mesh;
 public class CreateCylinder : MonoBehaviour
 {
     //[SerializeField] public float slices = 16, height = 7, radiusTop = 2, radiusBottom = 4;
+    float radius = 0.2f;
     Mesh mesh;
     //public Vector3 direction;
 
@@ -28,7 +30,74 @@ public class CreateCylinder : MonoBehaviour
 
     public void CreateGeometry(List<Branch> branches, float slices)
     {
+        /*create circle with the following formula:
+         *x = radius * cos(theta)
+         * y = radius * sin(theta)
+         * z = height
+         * theta runs from 0 to 2 * PI
+         */
+        List<Vector3> verticesList = new List<Vector3>();
+        List<int> triangleList = new List<int>();
+        Vector3[] vertices;
+        int[] triangles;
+        int baseIndex = verticesList.Count;
 
+        float x, y, z;
+        float theta = ((float)Mathf.PI * 2) / slices;
+
+        Quaternion directionAdjust = Quaternion.FromToRotation(Vector3.down, branches[0].direction);
+        
+        for(int i = 0; i <= slices; ++i)
+        {
+            x = radius * Mathf.Cos(i*theta);
+            y = 0;
+            z = radius * Mathf.Sin(i*theta);
+
+            Vector3 position = new Vector3(x, y, z);
+            position = directionAdjust * position;
+            position += branches[0].startPos;
+            verticesList.Add(position);
+        }
+
+        verticesList.Add(branches[0].startPos); //center vertex
+        int centerIndex = verticesList.Count-1;
+
+        for (int j = 0; j < slices; ++j)
+        {
+            triangleList.Add(centerIndex);
+            triangleList.Add(baseIndex + j + 1);
+            triangleList.Add(baseIndex + j);
+        }
+
+
+
+        /*for(int i = 1; i < branches.Count; i++)
+        {
+            directionAdjust = Quaternion.FromToRotation(Vector3.up, branches[i].direction);
+        }*/
+
+
+
+
+        //add to arrays
+
+        vertices = new Vector3[verticesList.Count];
+        for (int i = 0; i < verticesList.Count; i++)
+        {
+            vertices[i] = verticesList[i];
+        }
+        //tris
+        triangles = new int[triangleList.Count];
+        for (int i = 0; i < triangleList.Count; i++)
+        {
+            triangles[i] = triangleList[i];
+        }
+
+        //set it on the mesh
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        //optimise ?
         mesh.Optimize();
     }
 
